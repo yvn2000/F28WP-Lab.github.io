b3c97b9589b1149978ce0b3f56deb9e2aaf4f280
yvn2000 <145144435+yvn2000@users.noreply.github.com>
2023-10-14 16:48:41 +0400

    New form completed and added to contact.html
    A new form following the lab3 instructions was created and added to the contact.html page replacing the old form.

--- week3/scriptJS.js
+++ week3/scriptJS.js
@@ -1,4 +1,6 @@
 
+//Slide Show
+
 var slideIndex = 1;
 var oldSlide;
 showSlides(slideIndex);
@@ -33,7 +35,129 @@ function showSlides(n) {
         else {getSlide.style.display = "none";}
     }
 
-    
-
 }
+
+// Form
+
+    const form = document.getElementById('form');
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirmPassword');
+    var submittable = false;
+
+    const elementArray = [username, email, password, confirmPassword];
+    const messageArray = ["Username", "E-Mail", "Password", "Password"];
+
+    addEventListener('load', event => {
+      validateInputs();
+    })
+
+
+    form.addEventListener('submit', event => {
+
+      submittable = validateForm();
+
+      if (!submittable) {
+        event.preventDefault();         //prevents form from being sent
+      }                 
+      
+    })
+    
+    const setError = (element, errorMessage) => {                 //(element, text) are parameters
+      const inputControl = element.parentElement;
+      const errorDisplay = inputControl.querySelector('.error');
+
+      errorDisplay.innerText = errorMessage;
+      inputControl.classList.add('error');
+      inputControl.classList.remove('success');
+    }
+
+    const setSuccess = (element, successMessage) => {
+      const inputControl = element.parentElement;
+      const errorDisplay = inputControl.querySelector('.error');
+
+      errorDisplay.innerText = successMessage;
+      inputControl.classList.add('success');
+      inputControl.classList.remove('error');
+    };
+
+    function validateForm() {
+
+      var successCount = 0;
+
+      for (let i=0; i<elementArray.length; i++) {
+
+        if (elementArray[i].parentElement.classList.contains('success')) {
+          successCount++;
+        }
+
+        else {
+          return false;
+        }
+
+      }
+
+      if (successCount == elementArray.length) {
+        alert("Form Submitted!")
+        return true;
+      }
+
+    }
+
+
+    const validateInputs = () => {
+
+      for (let i=0; i<messageArray.length; i++) {
+        
+        elementArray[i].addEventListener("blur", (event) => {
+
+          if (elementArray[i].value.trim() === '') {
+            setError(elementArray[i], messageArray[i] + ' is required');
+          }
+
+          else if (elementArray[i]==email) {
+
+            var emailValue = elementArray[i].value.trimEnd();
+
+            if (!emailValue.includes('@') || !emailValue.includes('.com') || emailValue.includes(' ')) {
+              setError(elementArray[i], 'Valid ' + messageArray[i] + ' is required.   ex: 123@gmail.com');
+            }
+            else {
+              setSuccess(elementArray[i], '');
+            };
+
+          }
+
+          else if (elementArray[i]==password) {
+            var passwordValue = elementArray[i].value;
+
+            if (passwordValue.length<8) {
+              setError(elementArray[i], messageArray[i] + ' must be 8 characters long');
+            }
+            else {
+              setSuccess(elementArray[i], '');
+            };
+          }
+
+          else if (elementArray[i]==confirmPassword) {
+            var passwordValue = elementArray[i-1].value;
+            var confirmPasswordValue = elementArray[i].value;
+
+            if (confirmPasswordValue != passwordValue) {
+              setError(elementArray[i], messageArray[i] + 's do not match');
+            }
+            else {
+              setSuccess(elementArray[i], '');
+            };
+          }
+
+          else {
+            setSuccess(elementArray[i], '');
+          }
+          });
+
+      }
+
+    };
 
