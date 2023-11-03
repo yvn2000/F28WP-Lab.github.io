9f03c5c27a9857771c79aaeb2eb272b508fc9745
yvn2000 <145144435+yvn2000@users.noreply.github.com>
2023-11-03 10:39:22 +0400

    Added some functionality for the weather

--- week4/weather_website/app.js
+++ week4/weather_website/app.js
@@ -3,20 +3,21 @@ var weatherContainer = document.getElementById("weather-info");
 
 var btn = document.getElementById("btn");
 var city = document.getElementById("cityInput");
-console.log(city.innerHTML);
+console.log(city.value);
 
 btn.addEventListener("click", function() {
+    //weatherContainer.insertAdjacentHTML('beforeend' , city.value);
 
     var ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', 
-    'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}');
+    'https://api.openweathermap.org/data/2.5/weather?q=' +city.value+ '&appid=16f0bec95d207a891c1faef6b39f6cd2');
     ourRequest.onload = function() {
         //console.log(ourRequest.responseText);
 
         var ourData = JSON.parse(ourRequest.responseText);
 
         renderHTML(ourData);
-        btn.classList.add("hide-me");
+        //btn.classList.add("hide-me");
     };
     ourRequest.send();
 
@@ -27,7 +28,10 @@ function renderHTML(data){
 
     var htmlString = "";
 
+    htmlString += "<p>" + data[0].weather[0].description +  
+        ", </br> Is the current weather condition </br>" ;
     
+    htmlString += '.</p>';
 
     weatherContainer.insertAdjacentHTML('beforeend' , htmlString);
     
