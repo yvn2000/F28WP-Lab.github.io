6bd428050a2fac97a3139a396ca8476dc623efca
yvn2000 <145144435+yvn2000@users.noreply.github.com>
2023-11-03 10:03:11 +0400

    Button click adds  info for each country

--- week4/main.js
+++ week4/main.js
@@ -14,6 +14,7 @@ btn.addEventListener("click", function() {
         console.log(ourData[0]);
 
         renderHTML(ourData);
+        btn.classList.add("hide-me");
     };
     ourRequest.send();
 
@@ -21,6 +22,38 @@ btn.addEventListener("click", function() {
 
 
 function renderHTML(data){
-    var htmlString = "this is a test";
+
+    var htmlString = "";
+
+    for (i=0; i<data.length; i++){
+        htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + 
+        ", </br> Where you can enjoy indoor places like:" ;
+    
+
+        // Loop through the indoor places of the current city.
+        for (ii = 0; ii < data[i].places.indoor.length; ii++) {
+            if (ii == 0) {
+                htmlString += data[i].places.indoor[ii];
+            }
+            else {
+                htmlString += ", and " + data[i].places.indoor[ii];
+            }
+            }
+        htmlString += '. & enjoy outdoor places like: ';
+
+        // Loop through the outdoor places of the current city.
+        for (ii = 0; ii < data[i].places.outdoor.length; ii++) {
+            if (ii == 0) {
+                htmlString += data[i].places.outdoor[ii];
+            }
+            else {
+                htmlString += " and " + data[i].places.outdoor[ii];
+            }
+        }
+        htmlString += '.</p>';
+    }
+
     cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+    
+
 }
