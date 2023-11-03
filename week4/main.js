b0b687140aea0d1853d4f8ddea97f4d6fedc6df9
yvn2000 <145144435+yvn2000@users.noreply.github.com>
2023-11-03 09:52:24 +0400

    Added some ajax and json functionality to the button

--- week4/main.js
+++ week4/main.js
@@ -1,6 +1,26 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://yvn2000.github.io/F28WP-Lab.github.io/week4/cities1.json');
-ourRequest.onload = function() {
-console.log(ourRequest.responseText);
-};
-ourRequest.send();
+
+var cityContainer = document.getElementById("city-info");
+
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://yvn2000.github.io/F28WP-Lab.github.io/week4/cities1.json');
+    ourRequest.onload = function() {
+        //console.log(ourRequest.responseText);
+
+        var ourData = JSON.parse(ourRequest.responseText);
+        console.log(ourData[0]);
+
+        renderHTML(ourData);
+    };
+    ourRequest.send();
+
+})
+
+
+function renderHTML(data){
+    var htmlString = "this is a test";
+    cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+}
