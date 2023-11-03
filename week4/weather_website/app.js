8a8a06e8e573ee21dd5f893327b7a7754e5b53b5
yvn2000 <145144435+yvn2000@users.noreply.github.com>
2023-11-03 10:47:48 +0400

    yeah

--- week4/weather_website/app.js
+++ week4/weather_website/app.js
@@ -6,11 +6,15 @@ var city = document.getElementById("cityInput");
 console.log(city.value);
 
 btn.addEventListener("click", function() {
-    //weatherContainer.insertAdjacentHTML('beforeend' , city.value);
+    weatherContainer.insertAdjacentHTML('beforeend' , city.value);
 
     var ourRequest = new XMLHttpRequest();
+    
     ourRequest.open('GET', 
     'https://api.openweathermap.org/data/2.5/weather?q=' +city.value+ '&appid=16f0bec95d207a891c1faef6b39f6cd2');
+    
+    /*ourRequest.open('GET', 
+    'test.json');*/
     ourRequest.onload = function() {
         //console.log(ourRequest.responseText);
 
@@ -28,7 +32,7 @@ function renderHTML(data){
 
     var htmlString = "";
 
-    htmlString += "<p>" + data[0].weather[0].description +  
+    htmlString += "<p>" + data.weather[0].description +  
         ", </br> Is the current weather condition </br>" ;
     
     htmlString += '.</p>';
