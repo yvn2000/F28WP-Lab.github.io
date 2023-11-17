55a4dec364be986c6e5ee366cde18d54e1ecb6bf
yvn2000 <145144435+yvn2000@users.noreply.github.com>
2023-11-17 22:15:50 +0400

    Created sql database and subsequent conncection code

--- week5/index.js
+++ week5/index.js
@@ -1,6 +1,35 @@
 const express = require('express');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
 const app = express();
 const port = 5000;
+
+//const dotenv = require('require');
+dotenv.config({path: './.env'})
+
+
+const db = mysql.createConnection({
+    /*host : "localhost",
+    user : "root",
+    password : '',
+    database : 'user_auth'*/
+    host : process.env.DATABASE_HOST,
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE
+});
+
+
+db.connect((error)=>{
+    if (error){
+        console.log(error)
+    }
+    else {
+        console.log("MySQL Connected...")
+    }
+});
+
 
 
 app.get ("/", (req, res) => {
